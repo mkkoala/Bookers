780ad4c7e6f3fb1f7afee28ea840b10eb194600f
mkkoala <mako1105.96@gmail.com>
2019-12-09 19:11:14 +0900

    [Add] 4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,14 +3,32 @@ class BooksController < ApplicationController
   end
 
   def index
+  	@books = Book.all
+  	@book = Book.new
   end
 
   def show
+  	@book = Book.find(params[:id])
   end
 
   def new
+  	@book = Book.new
+  end
+
+  def create
+  	@book = Book.new(book_params)
+  	if @book.save
+  		redirect_to book_path(params[:id])
+  	else
+  		render action: :index
+  	end
   end
 
   def edit
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
   end
 end
